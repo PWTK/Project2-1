955bb945dcae346eb0e98484a4342ac6987063c3
Pongwasin Wintakorn <pongwasin@hotmail.com>
2016-11-18 17:26:24 +0700

    Fixing Thread bug

--- src/main/java/io/muic/dcom/p2/DataModel.java
+++ src/main/java/io/muic/dcom/p2/DataModel.java
@@ -1,8 +1,12 @@
 package io.muic.dcom.p2;
 
+//import java.util.ArrayList;
+//import java.util.List;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+//import java.util.stream.Collectors;
 
 public class DataModel {
     public static class ParcelObserved {
@@ -21,26 +25,57 @@ public class DataModel {
         public long getTimeStamp() { return timeStamp; }
     }
 
-    private List<ParcelObserved> transactions;
+    private ConcurrentHashMap<String, List> ParcelCollectors;
+    private ConcurrentHashMap<String, Long> StationCollectors;
+//    private List<ParcelObserved> transactions;
 
     DataModel() {
-        transactions = new ArrayList<>();
+        ParcelCollectors = new ConcurrentHashMap<>();
+        StationCollectors = new ConcurrentHashMap<>();
+//        transactions = new ArrayList<>();
     }
 
-    public synchronized void postObserve(String parcelId, String stationId, long timestamp) {
+    public void postObserve(String parcelId, String stationId, long timestamp) {
         ParcelObserved parcelObserved = new ParcelObserved(parcelId, stationId, timestamp);
-        transactions.add(parcelObserved);
+//        transactions.add(parcelObserved);
+        if (StationCollectors.containsKey(stationId)) {
+            StationCollectors.put(stationId, StationCollectors.get(stationId) + 1);
+        }
+
+        else {
+            StationCollectors.put(stationId, 1L);
+        }
+
+        if (ParcelCollectors.containsKey(parcelId)) {
+            ParcelCollectors.get(parcelId).add(parcelObserved);
+//            ParcelCollectors.put(parcelId, ParcelCollectors.get(parcelId).add(parcelObserved));
+        }
+        else {
+            List<ParcelObserved> parcel = new ArrayList<>();
+            parcel.add(parcelObserved);
+            ParcelCollectors.put(parcelId, parcel);
+        }
     }
 
-    public synchronized List<ParcelObserved> getParcelTrail(String parcelId) {
-        return transactions.stream()
-                .filter(observeEvent -> observeEvent.parcelId.equals(parcelId))
-                .collect(Collectors.toList());
+//    public synchronized List<ParcelObserved> getParcelTrail(String parcelId) {
+//        return transactions.stream()
+//                .filter(observeEvent -> observeEvent.parcelId.equals(parcelId))
+//                .collect(Collectors.toList());
+//    }
+
+    public  List<ParcelObserved> getParcelTrail(String parcelId) {
+        return new ArrayList<>(ParcelCollectors.get(parcelId));
+
     }
 
-    public synchronized long getStopCount(String stationId) {
-        return transactions.stream()
-                .filter(observeEvent -> observeEvent.stationId.equals(stationId))
-                .count();
+
+//    public synchronized long getStopCount(String stationId) {
+//        return transactions.stream()
+//                .filter(observeEvent -> observeEvent.stationId.equals(stationId))
+//                .count();
+//    }
+
+    public Long getStopCount(String stationId) {
+        return StationCollectors.get(stationId);
     }
 }
